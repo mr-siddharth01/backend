95b846d4377ac1506b297d81e942026671477323
mrsingh <mrsinghup52@gmail.com>
2025-08-02 20:00:05 +0530

    nothing

--- server.js
+++ server.js
@@ -2,13 +2,7 @@
 import express from 'express';
 import dotenv from 'dotenv';
 import cors from 'cors';
-
-app.use(cors({
-  origin: 'http://localhost:5174', // ✅ Use your frontend's origin during local dev
-  methods: ['GET'],
-  credentials: true
-}));
-
+app.use(cors());
 
 dotenv.config();
 const app = express();
